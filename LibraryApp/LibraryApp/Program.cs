3424d0827cb3fdd9d8ffb90b8821207fc90b73b4
irenaYo <irenatugushi997@gmail.com>
2026-02-19 20:57:29 +0200

    done main

--- LibraryApp/LibraryApp/Program.cs
+++ LibraryApp/LibraryApp/Program.cs
@@ -1,12 +1,14 @@
-﻿using LibraryApp.Services;
+﻿using LibraryApp.models;
+using LibraryApp.Services;
 class Program
 {
+    static LibraryService service = new LibraryService();
     static void Main()
     {
         int choice = 0;
-        while (choice != 12)
+        while (choice != 11)
         {
-            Console.WriteLine("1. Add an author\n" +
+            Console.WriteLine("\n1. Add an author\n" +
                               "2. Add a book\n" +
                               "3. Show all authors\n" +
                               "4. Show all books\n" +
@@ -16,8 +18,7 @@ class Program
                               "8. Return a book\n" +
                               "9. Delete a book\n" +
                               "10. Delete an author\n" +
-                              "11. Show statistics\n" +
-                              "12. Exit");
+                              "11. Exit");
             Console.Write("Choose an option: ");
             choice = int.Parse(Console.ReadLine());
             DoChoice(choice);
@@ -26,113 +27,59 @@ class Program
 
     private static void DoChoice(int choice)
     {
-        LibraryService service = new LibraryService();
         switch (choice)
         {
             case 1:
             {
-                Console.Write("Enter author's name: ");
-                string name = Console.ReadLine();
-                Console.Write("Enter author's country: ");
-                string country = Console.ReadLine();
-                Console.Write("Enter author's year of birth: ");
-                int  yearOfBirth = int.Parse(Console.ReadLine());
-                service.AddAuthor(name, country, yearOfBirth);
+                AddAnAuthor();
                 break;
             }
             case 2:
             {
-                //вызвать метод ShowAllAuthors
-
-                Console.Write("Enter author's number: ");
-                int authorId = int.Parse(Console.ReadLine());
-                Console.Write("Enter book's title: ");
-                string title = Console.ReadLine();
-                Console.Write("Enter book release year: ");
-                int year =  int.Parse(Console.ReadLine());
-                Console.Write("Enter the number of pages: ");
-                int pages = int.Parse(Console.ReadLine());
-                service.AddBook(title, year, pages, pages);
+                AddBook();
                 break;
             }
             case 3:
             {
-                var authors =  service.GetAllAuthors();
-                foreach (var author in authors)
-                {
-                    Console.WriteLine($"Author\n{author.Id}. {author.Name}, {author.Country}, {author.BirthYear}");
-                }
+                ShowAllAuthors();
                 break;
             }
             case 4:
             {
-                var books = service.GetAllBooks();
-                foreach (var book in books)
-                {
-                    Console.WriteLine($"Book\n{book.Id} ({book.Year}, {book.Pages} pages)");
-                }
+                ShowAllBooks();
                 break;
             }
             case 5:
             {
-                Console.Write("Enter author's name: ");
-                string name = Console.ReadLine();
-                var books = service.GetAllBooksByAuthor(name);
-                foreach (var book in books)
-                {
-                    Console.WriteLine($"Book\n{book.Id} ({book.Year}, {book.Pages} pages)");
-                }
+                ShowBooksByAuthor();
                 break;
             }
             case 6:
             {
-                Console.Write("Enter book's title: ");
-                string title = Console.ReadLine();
-                var book = service.GetBookByTitle(title);
-                if  (book != null)
-                    Console.WriteLine($"Book\n{book.Id} ({book.Year}, {book.Pages} pages)");
-                else
-                    Console.WriteLine("Book not found");
+                ShowBookByTitle();
                 break;
             }
             case 7:
             {
-                Console.Write("Enter book's title: ");
-                string title = Console.ReadLine();
-                service.GetBookByTitle(title);
-                Console.WriteLine("You took the book");
+                BorrowBook();
                 break;
             }
             case 8:
             {
-                Console.Write("Enter book's title: ");
-                string title = Console.ReadLine();
-                service.ReturnBook(title);
-                Console.WriteLine("You returned the book");
+                ReturnBook();
                 break;
             }
             case 9:
             {
-                Console.Write("Enter book's title: ");
-                string title = Console.ReadLine();
-                service.DeleteBook(title);
-                Console.WriteLine("You deleted the book");
+                DeleteBook();
                 break;
             }
             case 10:
             {
-                Console.WriteLine("Enter author's name: ");
-                string name = Console.ReadLine();
-                service.DeleteAuthor(name);
-                Console.WriteLine("You deleted the author");
+                DeleteAuthor();
                 break;
             }
             case 11:
-            {
-                //
-                break;
-            }
-            case 12:
             {
                 Console.WriteLine("Exiting...");
                 break;
@@ -142,6 +89,113 @@ class Program
                 Console.WriteLine("Invalid choice");
                 break;
             }
+        }
+    }
+
+    private static void AddAnAuthor()
+    {
+        Console.Write("Enter author's name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter author's country: ");
+        string country = Console.ReadLine();
+        Console.Write("Enter author's year of birth: ");
+        int  yearOfBirth = int.Parse(Console.ReadLine());
+        service.AddAuthor(name, country, yearOfBirth);
+    }
+
+    private static void AddBook()
+    {
+        ShowAllAuthors();
+        var authors =  service.GetAllAuthors();
+                
+        Console.Write("Enter author's number: ");
+        int authorNumber = int.Parse(Console.ReadLine());
+        int authorId = authors[authorNumber - 1].Id;
+        string title = InputTitle();
+        Console.Write("Enter book release year: ");
+        int year =  int.Parse(Console.ReadLine());
+        Console.Write("Enter the number of pages: ");
+        int pages = int.Parse(Console.ReadLine());
+        service.AddBook(title, year, pages, authorId);
+    }
+
+    private static void ShowAllAuthors()
+    {
+        var authors =  service.GetAllAuthors();
+        Console.WriteLine("Authors");
+        for (int i = 0; i < authors.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {authors[i].Name}, {authors[i].Country}, {authors[i].BirthYear}");
+        }
+    }
+
+    private static void ShowAllBooks()
+    {
+        var books = service.GetAllBooks();
+        Console.WriteLine("Books");
+        ShowBooks(books);
+    }
+
+    private static void ShowBooksByAuthor()
+    {
+        Console.Write("Enter author's name: ");
+        string name = Console.ReadLine();
+        var books = service.GetAllBooksByAuthor(name);
+        ShowBooks(books);
+    }
+
+    private static void ShowBookByTitle()
+    {
+        string title = InputTitle();
+        var book = service.GetBookByTitle(title);
+        Console.WriteLine($"{book.Title} ({book.Year}, {book.Pages} pages)");
+    }
+
+    private static void BorrowBook()
+    {
+        string title = InputTitle();
+        bool isFree = service.TakeBook(title);
+        if (isFree == false)
+            Console.WriteLine("Book is already borrowed");
+        else
+            Console.WriteLine("You took the book");
+    }
+
+    private static void ReturnBook()
+    {
+        string title = InputTitle();
+        service.ReturnBook(title);
+        Console.WriteLine("You returned the book");
+    }
+
+    private static void DeleteBook()
+    {
+        string title = InputTitle();
+        service.DeleteBook(title);
+        Console.WriteLine("You deleted the book");
+    }
+    
+    private static void DeleteAuthor()
+    {
+        Console.WriteLine("Enter author's name: ");
+        string name = Console.ReadLine();
+        service.DeleteAuthor(name);
+        Console.WriteLine("You deleted the author");
+    }
+    
+    private static string InputTitle()
+    {
+        Console.Write("Enter title: ");
+        string title = Console.ReadLine();
+        return title;
+    }
+    
+
+    private static void ShowBooks(List<Book> books)
+    {
+        for (int i = 0; i < books.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {books[i].Title} ({books[i].Year}, {books[i].Pages} pages)");
         }
     }
 }
