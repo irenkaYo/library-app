3af2652bb8e5a82236c21fa0c317d2761927c429
irenaYo <irenatugushi997@gmail.com>
2026-02-19 15:32:38 +0200

    add for DB

---
+++ LibraryApp/LibraryApp/Migrations/20260219125259_InitialCreateTablesAuthorAndBook.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LibraryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateTablesAuthorAndBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: false),
+                    BirthYear = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Year = table.Column<int>(type: "integer", nullable: false),
+                    Pages = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    AuthorId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "BirthYear", "Country", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1828, "Russia", "Lev Tolstoi" },
+                    { 2, 1821, "Russia", "Fyodor Dostoevsky" },
+                    { 3, 1965, "United Kingdom", "Joanne Rowling" },
+                    { 4, 1903, "United Kingdom", "Jorj Oruell" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "Pages", "Status", "Title", "Year" },
+                values: new object[,]
+                {
+                    { 1, 1, 1225, 0, "Peace and war", 1869 },
+                    { 2, 1, 864, 0, "Anna Karenina", 1877 },
+                    { 3, 2, 671, 0, "Crime and Punishment", 1866 },
+                    { 4, 2, 796, 0, "The Brothers Karamazov", 1880 },
+                    { 5, 3, 223, 0, "Harry Potter and the Philosopher's Stone", 1977 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
